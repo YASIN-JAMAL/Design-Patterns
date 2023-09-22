61b3036bfb0d3fd66268dc9fbcc7388f3d39c800
YASIN-JAMAL <103166736+YASIN-JAMAL@users.noreply.github.com>
2023-09-22 09:26:44 +0500

    Singleton pattern
    in progress

---
+++ SingletonLab/src/singletonlab/SingletonLab.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package singletonlab;
+
+/**
+ *
+ * @author fa20-bse-172
+ */
+public class SingletonLab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
