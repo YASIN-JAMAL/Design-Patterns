418d90f097602b05cee8b2c91c289d8176e3710d
YASIN-JAMAL <103166736+YASIN-JAMAL@users.noreply.github.com>
2023-10-13 05:24:03 +0500

    Iterator Pattern is added
    Iterator Pattern is added

--- Iterator/src/gof/Iterator.java
+++ Iterator/src/gof/Iterator.java
@@ -3,15 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Iterator;
+package gof;
 
 /**
  *
  * @author fa20-bse-172
  */
 public interface Iterator {
- 
-   public boolean hasNext();
+     public boolean hasNext();
    public Object next();
 }
-
